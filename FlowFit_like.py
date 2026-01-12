track data's input normalization is fixed!`

FlowFit_like.py
@@ -48,20 +48,25 @@ def FlowFit_update(model_states, optimiser_fn, equation_fn, dynamic_params, stat
         def local_loss(p):
             return equation_fn(p, all_params, idx, dx, dy, dz, xu, xv, xw, pv, model_fn)
         val, grad = jax.value_and_grad(local_loss)(d_p)
-        updates, new_state = optimiser_fn(grad, state, d_p, value=val, grad=grad, value_fn=local_loss)
+        grad_projection = projection_fn(grad)[0]
+        updates, new_state = optimiser_fn(grad_projection, state, d_p, value=val, grad=grad_projection, value_fn=local_loss)
         new_param = optax.apply_updates(d_p, updates)
         return new_param, new_state, val
 
     vmap_fn = jax.vmap(single_update, in_axes=(0, state_in_axes, 0, 0, 0, 0, 0), out_axes=(0, state_out_axes, 0))
-    
+    #dynamic_zero = dynamic_params[0,:,:,:,:]
+    #lossval, grad = value_and_grad(equation_fn, argnums=0)(dynamic_params, all_params, index_list[0,:,:,:], dx, dy, dz, xu_list[0,:,:], xv_list[0,:,:], xw_list[0,:,:], particle_vel[0,:,:], model_fn)
+    #updates, new_state = optimiser_fn(grad, model_states, dynamic_params)
+    #dynamic_params = optax.apply_updates(dynamic_params, updates)
     new_params, new_states, lossvals = vmap_fn(dynamic_params, model_states, index_list, xu_list, xv_list, xw_list, particle_vel)
+    
     def fix_scalars(leaf, in_axis):
         if in_axis is None and hasattr(leaf, 'ndim') and leaf.ndim > 0:
             return leaf[0] 
         return leaf
 
     new_states = jax.tree_util.tree_map(fix_scalars, new_states, state_in_axes)
-
+    #return lossval, new_state, dynamic_params
     return lossvals, new_states, new_params
 
 
@@ -92,6 +97,7 @@ class FlowFit3(FlowFitbase):
         optimiser_fn = optimiser.update
         model_fn = self.c.prediction.velocity_pred
         dynamic_params = all_params["projection"].pop("coefficients")
+        #dynamic_params = dynamic_params[0,:,:,:,:]
         equation_fn = self.c.equation.Loss
         report_fn = self.c.equation.Loss_report
         projection_fn = self.c.projection.helmholtz_hodge_decomposition
@@ -162,18 +168,19 @@ class FlowFit3(FlowFitbase):
         update = FlowFit_update.lower(model_states, optimiser_fn, equation_fn, dynamic_params, static_params, static_keys, index_list, index_mask, dx, dy, dz, 
                    xu_list, xu_mask, xv_list, xv_mask, xw_list, xw_mask, particle_vel, particle_vel_mask, particle_acc, projection_fn, model_fn).compile()
         i = 0
-        for i in tqdm(range(100000)):
+        
+        for i in tqdm(range(10000)):
             lossvals, model_states, dynamic_params = update(model_states, dynamic_params, static_params, index_list, index_mask, dx, dy, dz, 
                                                                 xu_list, xu_mask, xv_list, xv_mask, xw_list, xw_mask, particle_vel, particle_vel_mask, particle_acc)
             
             self.report(i, report_fn, dynamic_params, all_params, index_list, index_mask, dx, dy, dz, 
                                                                 xu_list, xu_mask, xv_list, xv_mask, xw_list, xw_mask, particle_vel, particle_vel_mask, particle_acc, 
                                                                 self.c.optimization_init_kwargs["save_step"], model_fn)
-            #self.save_model(i, dynamic_params, all_params, self.c.optimization_init_kwargs["save_step"], model_fn)
+            
+            self.save_model(i, dynamic_params, all_params, self.c.optimization_init_kwargs["save_step"], model_fn)
             i +=1
 
-        print(dynamic_params)
-        #print(grad)
+        
         return
     
     def save_model(self, i, dynamic_params, all_params, save_step, model_fns):
@@ -195,15 +202,14 @@ class FlowFit3(FlowFitbase):
             #vmap_model = jax.vmap(model_fns, in_axes=(0, 0, None, None, None, 0, 0, 0))
             #u_pred, v_pred, w_pred = vmap_model(dynamic_params, index_list, dx, dy, dz, xu_list, xv_list, xw_list)
             dynamic_params_new = np.array(dynamic_params)
-            u_pred, v_pred, w_pred = model_fns(dynamic_params[0], index_list[0], dx, dy, dz, xu_list[0], xv_list[0], xw_list[0])
-            u_error = jnp.sqrt(jnp.mean((u_pred - particle_vel[:,:,0])**2)/jnp.mean(particle_vel[0,:,0]**2))
-            v_error = jnp.sqrt(jnp.mean((v_pred - particle_vel[:,:,1])**2)/jnp.mean(particle_vel[0,:,1]**2))
-            w_error = jnp.sqrt(jnp.mean((w_pred - particle_vel[:,:,2])**2)/jnp.mean(particle_vel[0,:,2]**2))
+            u_pred, v_pred, w_pred = model_fns(dynamic_params[0,:,:,:,:], index_list[0,:,:,:], dx, dy, dz, xu_list[0,:,:], xv_list[0,:,:], xw_list[0,:,:])
+            u_error = jnp.sqrt(jnp.mean((u_pred - particle_vel[0,:,0])**2)/jnp.mean(particle_vel[0,:,0]**2))
+            v_error = jnp.sqrt(jnp.mean((v_pred - particle_vel[0,:,1])**2)/jnp.mean(particle_vel[0,:,1]**2))
+            w_error = jnp.sqrt(jnp.mean((w_pred - particle_vel[0,:,2])**2)/jnp.mean(particle_vel[0,:,2]**2))
             #if v_pred.shape[1] == 5:
             #    T_error = jnp.sqrt(jnp.mean((all_params["data"]["T_ref"]*v_pred[:,4] - e_batch_T)**2)/jnp.mean(e_batch_T**2))
 
             #Losses = report_fn(dynamic_params, all_params, g_batch, p_batch, v_batch, b_batch, model_fns)
-            print('check3')
             print(f"step_num : {i:<{12}} u_error : {u_error:<{12}.{5}} v_error : {v_error:<{12}.{5}} w_error : {w_error:<{12}.{5}}")
             with open(self.c.report_out_dir + "reports.txt", "a") as f:
                 f.write(f"{i:<{12}} {u_error:<{12}.{5}} {v_error:<{12}.{5}} {w_error:<{12}.{5}}\n")